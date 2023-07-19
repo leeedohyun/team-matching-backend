8d96b81203e67d1677e097eaeaf82cf4132a5afc
leeedohyun <ldh19738@gmail.com>
2023-07-19 16:42:11 +0900

    init

---
+++ src/main/java/server/teammatching/TeamMatchingApplication.java
@@ -0,0 +1,13 @@
+package server.teammatching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamMatchingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamMatchingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/server/teammatching/TeamMatchingApplicationTests.java
@@ -0,0 +1,13 @@
+package server.teammatching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamMatchingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
