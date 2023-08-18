e425f0eca04848d6ac00debe23f7bed6090bea1a
leeedohyun <ldh19738@gmail.com>
2023-08-18 17:00:43 +0900

    feat 로그아웃 기능 구현

--- src/main/java/server/teammatching/auth/AuthController.java
+++ src/main/java/server/teammatching/auth/AuthController.java
@@ -3,7 +3,10 @@ package server.teammatching.auth;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +15,7 @@ import server.teammatching.dto.request.LoginRequest;
 
 @RestController
 @RequiredArgsConstructor
-@Api(tags = {"로그인 API"})
+@Api(tags = {"로그인/로그아웃 API"})
 @RequestMapping("/auth")
 public class AuthController {
 
@@ -26,5 +29,15 @@ public class AuthController {
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
+    }
+
+    @ApiOperation(value = "로그아웃 API")
+    @PostMapping("/logout")
+    public ResponseEntity<String> logout(@AuthenticationPrincipal PrincipalDetails principal) {
+        if (principal != null) {
+            SecurityContextHolder.clearContext();
+            return ResponseEntity.ok("로그아웃이 정상적으로 처리되었습니다.");
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("이미 로그아웃된 상태입니다.");
     }
 }
