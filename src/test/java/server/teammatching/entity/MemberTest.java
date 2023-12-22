52c316c359ea3a97a79c858789e394241265d5db
leeedohyun <ldh19738@gmail.com>
2023-12-23 00:05:59 +0900

    test: 테스트 코드 중복 부분 메서드 추출

--- src/test/java/server/teammatching/entity/MemberTest.java
+++ src/test/java/server/teammatching/entity/MemberTest.java
@@ -23,51 +23,33 @@ class MemberTest {
     @Test
     @DisplayName("이메일이 null이면 수정되지 않는다.")
     void notUpdateEmailByNull() {
-        // given
-        final MemberRequestDto dto = MemberRequestDto.builder()
-                .password("1234")
-                .email("abd@gmail.com")
-                .build();
-        final Member member = Member.createMember(dto, passwordEncoder);
-
-        // when
-        member.updateEmail(null);
-
-        // then
-        assertThat(member.getEmail()).isEqualTo("abd@gmail.com");
+        assertEmailUpdate("abc@gmail.com", null, "abc@gmail.com");
     }
 
     @Test
     @DisplayName("이메일이 빈 문자열이면 수정되지 않는다.")
     void notUpdateEmailByEmptyString() {
-        // given
-        final MemberRequestDto dto = MemberRequestDto.builder()
-                .password("1234")
-                .email("abd@gmail.com")
-                .build();
-        final Member member = Member.createMember(dto, passwordEncoder);
-
-        // when
-        member.updateEmail("");
-
-        // then
-        assertThat(member.getEmail()).isEqualTo("abd@gmail.com");
+        assertEmailUpdate("abd@gmail.com", "", "abd@gmail.com");
     }
 
     @Test
     @DisplayName("이메일이 null이나 빈 문자열이 아니면 수정된다.")
     void updateEmail() {
+        assertEmailUpdate("abd@gmail.com", "efg@naver.com", "efg@naver.com");
+    }
+
+    private void assertEmailUpdate(final String email, final String emailToUpdate, final String expected) {
         // given
         final MemberRequestDto dto = MemberRequestDto.builder()
                 .password("1234")
-                .email("abd@gmail.com")
+                .email(email)
                 .build();
         final Member member = Member.createMember(dto, passwordEncoder);
 
         // when
-        member.updateEmail("efg@naver.com");
+        member.updateEmail(emailToUpdate);
 
         // then
-        assertThat(member.getEmail()).isEqualTo("efg@naver.com");
+        assertThat(member.getEmail()).isEqualTo(expected);
     }
 }
