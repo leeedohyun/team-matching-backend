47940f12a05732b05163772a391c8d9a68f01eba
leeedohyun <ldh19738@gmail.com>
2024-01-04 23:02:28 +0900

    refactor: 줄바꿈

--- src/test/java/server/teammatching/service/ProjectServiceTest.java
+++ src/test/java/server/teammatching/service/ProjectServiceTest.java
@@ -38,38 +38,56 @@ class ProjectServiceTest extends PostServiceTest {
         // given
         final String memberId = "hello";
 
-        given(memberRepository.findByLoginId(any())).willThrow(MemberNotFoundException.class);
+        given(memberRepository.findByLoginId(any()))
+                .willThrow(MemberNotFoundException.class);
 
         // when
         // then
-        final ProjectRequestDto requestDto = ProjectRequestDto.builder().title("프로젝트 모집합니다.").backendNumber(1)
-                .field("백엔드, 디자이너").designerNumber(1).frontendNumber(0).recruitNumber(2)
-                .content("안녕하세요. 프로젝트 기획을 하게 되었는데, 같이 할 팀원 구합히다.,").build();
+        final ProjectRequestDto requestDto = ProjectRequestDto.builder()
+                .title("프로젝트 모집합니다.")
+                .backendNumber(1)
+                .field("백엔드, 디자이너")
+                .designerNumber(1)
+                .frontendNumber(0)
+                .recruitNumber(2)
+                .content("안녕하세요. 프로젝트 기획을 하게 되었는데, 같이 할 팀원 구합히다.,")
+                .build();
 
-        assertThatThrownBy(() -> projectService.create(memberId, requestDto)).isInstanceOf(
-                MemberNotFoundException.class);
+        assertThatThrownBy(() -> projectService.create(memberId, requestDto))
+                .isInstanceOf(MemberNotFoundException.class);
     }
 
     @Test
     void 프로젝트_생성_시_전체_인원과_분야별_인원_수의_합이_일치하지_않으면_예외_발생() {
         // given
-        given(memberRepository.findByLoginId(any())).willReturn(Optional.of(leader));
+        given(memberRepository.findByLoginId(any()))
+                .willReturn(Optional.of(leader));
 
         // when
         // then
-        final ProjectRequestDto requestDto = ProjectRequestDto.builder().backendNumber(1).designerNumber(1)
-                .frontendNumber(0).recruitNumber(8).build();
+        final ProjectRequestDto requestDto = ProjectRequestDto.builder()
+                .backendNumber(1)
+                .designerNumber(1)
+                .frontendNumber(0)
+                .recruitNumber(8)
+                .build();
 
-        assertThatThrownBy(() -> projectService.create(leader.getLoginId(), requestDto)).isInstanceOf(
-                InsufficientMembersException.class);
+        assertThatThrownBy(() -> projectService.create(leader.getLoginId(), requestDto))
+                .isInstanceOf(InsufficientMembersException.class);
     }
 
     @Test
     void 프로젝트_생성() {
         // given
-        final ProjectRequestDto requestDto = ProjectRequestDto.builder().title("프로젝트 모집합니다.").backendNumber(1)
-                .field("백엔드, 디자이너").designerNumber(1).frontendNumber(0).recruitNumber(2)
-                .content("안녕하세요. 프로젝트 기획을 하게 되었는데, 같이 할 팀원 구합히다.,").build();
+        final ProjectRequestDto requestDto = ProjectRequestDto.builder()
+                .title("프로젝트 모집합니다.")
+                .backendNumber(1)
+                .field("백엔드, 디자이너")
+                .designerNumber(1)
+                .frontendNumber(0)
+                .recruitNumber(2)
+                .content("안녕하세요. 프로젝트 기획을 하게 되었는데, 같이 할 팀원 구합히다.,")
+                .build();
 
         final Post project = Post.createProject(requestDto.getTitle(), requestDto.getField(), requestDto.getTechStack(),
                 requestDto.getContent(), requestDto.getRecruitNumber(), requestDto.getDesignerNumber(),
@@ -88,7 +106,8 @@ class ProjectServiceTest extends PostServiceTest {
     @Test
     void 프로젝트_삭제_시_존재하지_않는_프로젝트이면_예외_발생() {
         // given
-        given(postRepository.findById(any())).willThrow(PostNotFoundException.class);
+        given(postRepository.findById(any()))
+                .willThrow(PostNotFoundException.class);
 
         // when
         // then
@@ -102,7 +121,8 @@ class ProjectServiceTest extends PostServiceTest {
 
         // when
         // then
-        assertThatThrownBy(() -> projectService.findOne(any())).isInstanceOf(PostNotFoundException.class);
+        assertThatThrownBy(() -> projectService.findOne(any()))
+                .isInstanceOf(PostNotFoundException.class);
     }
 
     @Test
@@ -162,7 +182,8 @@ class ProjectServiceTest extends PostServiceTest {
     }
 
     private List<Post> createProjects() {
-        final Post project1 = Post.createProject("프로젝트 모집", "백엔드", "자바, 스프링", "안녕하세요. 프로젝트 팀원 모집합니다.", 2, 0, 2, 0, leader);
+        final Post project1 = Post.createProject("프로젝트 모집", "백엔드", "자바, 스프링", "안녕하세요. 프로젝트 팀원 모집합니다.", 2, 0, 2, 0,
+                leader);
         final Post project2 = Post.createProject("프로젝트 모집", "디자이너", "피그마", "안녕하세요. 프로젝트 팀원 모집합니다.", 1, 1, 0, 0, leader);
         return List.of(project1, project2);
     }
