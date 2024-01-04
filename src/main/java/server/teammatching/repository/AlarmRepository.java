eab4cc9f97cf864ef52c33e17dd31afb46b7d4cd
leeedohyun <ldh19738@gmail.com>
2024-01-04 22:42:42 +0900

    refactor: final 키워드 추가

--- src/main/java/server/teammatching/repository/AlarmRepository.java
+++ src/main/java/server/teammatching/repository/AlarmRepository.java
@@ -1,12 +1,13 @@
 package server.teammatching.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import server.teammatching.entity.Alarm;
 import server.teammatching.entity.Member;
 
-import java.util.List;
-
 public interface AlarmRepository extends JpaRepository<Alarm, Long> {
 
-    List<Alarm> findByMember(Member member);
+    List<Alarm> findByMember(final Member member);
 }
