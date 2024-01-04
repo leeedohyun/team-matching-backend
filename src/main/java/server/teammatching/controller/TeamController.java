cd150e4253fda5fdd3d93b23f7ad21b898c69e4e
leeedohyun <ldh19738@gmail.com>
2024-01-04 21:36:09 +0900

    refactor: final 키워드 추가

--- src/main/java/server/teammatching/controller/TeamController.java
+++ src/main/java/server/teammatching/controller/TeamController.java
@@ -33,51 +33,51 @@ public class TeamController {
 
     @ApiOperation(value = "팀 생성 API")
     @PostMapping("/new")
-    public ResponseEntity<TeamAndStudyCreateResponseDto> create(@RequestBody TeamAndStudyCreateRequestDto form,
-                                                                @AuthenticationPrincipal PrincipalDetails principal) {
+    public ResponseEntity<TeamAndStudyCreateResponseDto> create(@RequestBody final TeamAndStudyCreateRequestDto form,
+                                                                @AuthenticationPrincipal final PrincipalDetails principal) {
         AuthenticationUtils.validateAuthentication(principal);
-        TeamAndStudyCreateResponseDto teamAndStudyCreateResponseDto = teamService.create(form, principal.getUsername());
+        final TeamAndStudyCreateResponseDto teamAndStudyCreateResponseDto = teamService.create(form, principal.getUsername());
         return ResponseEntity.created(URI.create(String.format("/new/%s", teamAndStudyCreateResponseDto.getPostId())))
                 .body(teamAndStudyCreateResponseDto);
     }
 
     @ApiOperation(value = "팀 정보 수정 API")
     @PatchMapping("/{id}")
-    public ResponseEntity<TeamAndStudyCreateResponseDto> update(@PathVariable("id") Long postId,
-                                                                @AuthenticationPrincipal PrincipalDetails principal,
-                                                                @RequestBody TeamAndStudyCreateRequestDto requestDto) {
+    public ResponseEntity<TeamAndStudyCreateResponseDto> update(@PathVariable("id") final Long postId,
+                                                                @AuthenticationPrincipal final PrincipalDetails principal,
+                                                                @RequestBody final TeamAndStudyCreateRequestDto requestDto) {
         AuthenticationUtils.validateAuthentication(principal);
-        TeamAndStudyCreateResponseDto responseDto = teamService.update(postId, requestDto);
+        final TeamAndStudyCreateResponseDto responseDto = teamService.update(postId, requestDto);
         return ResponseEntity.ok(responseDto);
     }
 
     @ApiOperation(value = "모든 팀 조회 API")
-    @GetMapping("")
+    @GetMapping
     public ResponseEntity<List<TeamAndStudyCreateResponseDto>> checkAllTeams() {
-        List<TeamAndStudyCreateResponseDto> allTeamsResponse = teamService.checkAllTeams();
+        final List<TeamAndStudyCreateResponseDto> allTeamsResponse = teamService.checkAllTeams();
         return ResponseEntity.ok(allTeamsResponse);
     }
 
     @ApiOperation(value = "팀 상세 조회")
-    @GetMapping("/check/{id}")
-    public ResponseEntity<TeamAndStudyCreateResponseDto> checkOne(@PathVariable("id") Long teamId) {
-        TeamAndStudyCreateResponseDto responseDto = teamService.findOne(teamId);
+    @GetMapping("/check/{teamId}")
+    public ResponseEntity<TeamAndStudyCreateResponseDto> checkOne(@PathVariable final Long teamId) {
+        final TeamAndStudyCreateResponseDto responseDto = teamService.findOne(teamId);
         return ResponseEntity.ok(responseDto);
     }
 
     @ApiOperation(value = "회원이 생성한 팀 조회 API")
     @GetMapping("/{id}")
-    public ResponseEntity<List<TeamAndStudyCreateResponseDto>> checkMemberTeams(@PathVariable("id") String loginId,
-                                                                                @AuthenticationPrincipal PrincipalDetails principal) {
+    public ResponseEntity<List<TeamAndStudyCreateResponseDto>> checkMemberTeams(@PathVariable("id") final String loginId,
+                                                                                @AuthenticationPrincipal final PrincipalDetails principal) {
         AuthenticationUtils.validateAuthentication(principal);
-        List<TeamAndStudyCreateResponseDto> allMemberTeamsResponse = teamService.checkMemberTeams(loginId);
+        final List<TeamAndStudyCreateResponseDto> allMemberTeamsResponse = teamService.checkMemberTeams(loginId);
         return ResponseEntity.ok(allMemberTeamsResponse);
     }
 
     @ApiOperation(value = "팀 삭제 API")
     @DeleteMapping("/{id}/delete")
-    public ResponseEntity<String> delete(@PathVariable("id") Long teamId,
-                                         @AuthenticationPrincipal PrincipalDetails principal) {
+    public ResponseEntity<String> delete(@PathVariable("id") final Long teamId,
+                                         @AuthenticationPrincipal final PrincipalDetails principal) {
         AuthenticationUtils.validateAuthentication(principal);
         teamService.delete(teamId);
         return ResponseEntity.ok("정상적으로 삭제되었습니다.");
